6c942451e84b3445798d1b1285e280670a538ead
RobertCSPA <robertf127@nycstudents.net>
2025-05-14 16:28:37 +0000

    date

--- Event.java
+++ Event.java
@@ -1,8 +1,12 @@
 class Event{
     private int day;
+    private int month;
+    private int year;
     private String name;
-    public Event(int d, String n){
+    public Event(int d, int m, int y, String n){
         day = d;
+        month = m;
+        year = y;
         name = n;
     }
     public String getName(){
@@ -11,10 +15,22 @@ class Event{
     public int getDay(){
         return day;
     }
+    public int getMonth(){
+        return month;
+    }
+    public int getYear(){
+        return year;
+    }
     public void setName(String s){
         name = s;
     }
     public void setDay(int d){
         day = d;
+    }
+    public void setMonth(int m){
+        month = m;
+    }
+    public void setYear(int y){
+        year = y;
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,26 @@ import java.util.ArrayList;
 public class Main{
     public static void main(String[] args) {
         ArrayList<Event> eventList = new ArrayList<Event>();
-        
+        ArrayList<Event> sortedEventList = new ArrayList<Event>();
+        while(eventList.size() != 0){
+            Event nearest = eventList.get(0);
+            int index = 0;
+            for(int i = 0; i < eventList.size(); i++){
+                if(eventList.get(i).getYear() < nearest.getYear()){
+                    nearest = eventList.get(i);
+                    index = i;
+                }
+                else if(eventList.get(i).getMonth() < nearest.getMonth()){
+                    nearest = eventList.get(i);
+                    index = i;
+                }
+                else if(eventList.get(i).getDay() < nearest.getDay()){
+                    nearest = eventList.get(i);
+                    index = i;
+                }
+            }
+            sortedEventList.add(nearest);
+            eventList.remove(index);
+        }
     }
 }
