1c85607412e97ec02774921580e635d3c7b2dc1e
RobertCSPA <robertf127@nycstudents.net>
2025-06-02 20:03:02 +0000

    scanner

--- Main.java
+++ Main.java
@@ -12,87 +12,66 @@ public class Main{
             String request = s.nextLine();
 
             if(request.equals("make a new event")){
-                Scanner a = new Scanner(System.in);
-                System.out.println("month: ");
-                int month = a.nextInt();
-                Scanner b = new Scanner(System.in);
-                System.out.println("day:");
-                int day = b.nextInt();
-                Scanner c = new Scanner(System.in);
-                System.out.println("year:");
-                int year = c.nextInt();
-                Scanner d = new Scanner(System.in);
-                System.out.println("name:");
-                String name = d.nextLine();
+                System.out.print("month: ");
+                int month = s.nextInt();
+                System.out.print("day: ");
+                int day = s.nextInt();
+                System.out.print("year:");
+                int year = s.nextInt();
+                System.out.print("name: ");
+                String name = s.nextLine();
                 Event ev = new Event(month, day, year, name);
                 if (eventList.size() == 0)eventList.add(ev);
                 else{
                     int index = eventList.size();
                     for(int i = 0; i < eventList.size(); i++){
-                        if(eventList.get(i).getYear() < ev.getYear()){
-
-                        index = i;
-                    }
-                    else if(eventList.get(i).getMonth() < ev.getMonth()){
-
-                        index = i;
-                    }
-                    else if(eventList.get(i).getDay() < ev.getDay()){
-                        index = i;
-                    }
+                        if (ev.getYear() < eventList.get(i).getYear() ||
+                        (ev.getYear() == eventList.get(i).getYear() && ev.getMonth() < eventList.get(i).getMonth()) ||
+                        (ev.getYear() == eventList.get(i).getYear() && ev.getMonth() == eventList.get(i).getMonth() && ev.getDay() < eventList.get(i).getDay())) {
+                            index = i;
+                            break;
+                        }
                     }
                     eventList.add(index, ev);
                 }
             }
 
             if(request.equals("make a new important event")){
-                Scanner a = new Scanner(System.in);
-                System.out.println("month: ");
-                int month = a.nextInt();
-                Scanner b = new Scanner(System.in);
-                System.out.println("day:");
-                int day = b.nextInt();
-                Scanner c = new Scanner(System.in);
-                System.out.println("year:");
-                int year = c.nextInt();
-                Scanner d = new Scanner(System.in);
-                System.out.println("name:");
-                String name = d.nextLine();
-                Scanner e = new Scanner(System.in);
-                System.out.println("reason:");
-                String reason = e.nextLine();
+                System.out.print("month: ");
+                int month = s.nextInt();
+                System.out.print("day: ");
+                int day = s.nextInt();
+                System.out.print("year: ");
+                int year = s.nextInt();
+                System.out.print("name: ");
+                String name = s.nextLine();
+                System.out.print("reason: ");
+                String reason = s.nextLine();
                 ImportantEvent ev = new ImportantEvent(month, day, year, name, reason);
                 if (importantEventList.size() == 0)importantEventList.add(ev);
                 else{
-                    int index = importantEventList.size()-1;
-                    for(int i = 0; i < importantEventList.size(); i++){
-                        if(importantEventList.get(i).getYear() < ev.getYear()){
-
-                        index = i;
+                    int index = importantEventList.size();
+                    for (int i = 0; i < importantEventList.size(); i++) {
+                        if (ev.getYear() < importantEventList.get(i).getYear() ||
+                            (ev.getYear() == importantEventList.get(i).getYear() && ev.getMonth() < importantEventList.get(i).getMonth()) ||
+                            (ev.getYear() == importantEventList.get(i).getYear() && ev.getMonth() == importantEventList.get(i).getMonth() && ev.getDay() < importantEventList.get(i).getDay())) {
+                            index = i;
+                            break;
+                            }
+                        }
+                        importantEventList.add(index, ev);
                     }
-                    else if(importantEventList.get(i).getMonth() < ev.getMonth()){
-
-                        index = i;
-                    }
-                    else if(importantEventList.get(i).getDay() < ev.getDay()){
-                        index = i;
-                    }
-                    }
-                    importantEventList.add(index, ev);
                 }
-            }
 
             if (request.equals("search event")){
-                Scanner a = new Scanner(System.in);
-                System.out.println("how would you like to search: ");
-                String method = a.nextLine();
+                System.out.print("how would you like to search: ");
+                String method = s.nextLine();
                 if(method.equals("name")){
-                    Scanner n = new Scanner(System.in);
-                    System.out.println("what is the name of the event?: ");
-                    String name = n.nextLine();
+                    System.out.print("what is the name of the event?: ");
+                    String name = s.nextLine();
                     boolean dne = true;
                     for (Event e:eventList){
-                        if (e.getName().equals(name)){
+                        if (e.getName().indexOf(name)!=0){
                             System.out.println(e);
                             dne = false;
                         }
@@ -108,15 +87,12 @@ public class Main{
                     if(dne2)System.out.println("no such event exists with that name in the important events");
                 }
                 else{
-                    Scanner m = new Scanner(System.in);
-                    System.out.println("what is the month of the event?: ");
-                    int month = m.nextInt();
-                    Scanner d = new Scanner(System.in);
-                    System.out.println("what is the day of the event?: ");
-                    int day = d.nextInt();
-                    Scanner y = new Scanner(System.in);
-                    System.out.println("what is the year of the event?: ");
-                    int year = y.nextInt();
+                    System.out.print("what is the month of the event?: ");
+                    int month = s.nextInt();
+                    System.out.print("what is the day of the event?: ");
+                    int day = s.nextInt();
+                    System.out.print("what is the year of the event?: ");
+                    int year = s.nextInt();
                     boolean dne = true;
                     for (Event e:eventList){
                         if (e.getDay() == day){
@@ -141,17 +117,15 @@ public class Main{
             }
 
             if(request.equals("remove normal")){
-                Scanner i = new Scanner(System.in);
-                System.out.println("which element would you like to remove: ");
-                int index = i.nextInt();
-                eventList.remove(i);
+                System.out.print("which element would you like to remove: ");
+                int index = s.nextInt();
+                eventList.remove(index);
             }
 
             if(request.equals("remove important")){
-                Scanner i = new Scanner(System.in);
-                System.out.println("which element would you like to remove: ");
-                int index = i.nextInt();
-                importantEventList.remove(i);
+                System.out.print("which element would you like to remove: ");
+                int index = s.nextInt();
+                importantEventList.remove(index);
             }
 
             if(request.equals("print events")){
